985c53e613d018a4f191048c1e053dc4cab439a0
SurfyGalaxy <ashton3141@gmail.com>
2026-09-15 23:21:54 +1000

    idk its late

--- equation.gd
+++ equation.gd
@@ -1,31 +1,22 @@
-extends Node
+extends Node2D
 
-var target
-var values = []
-var selected_value
+var operators = ["+", "-", "*", "<div>"]
+var equation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	equation = generate(2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	var temp = $"../".equation
-	if temp:
-		for value in temp:
-			if value not in ["+", "-", "*", "<div>"]:
-				values.append(value)
-	else:
-		values = []
-	
-	if target:
-		selected_value = values[int(target) - 1]
-		target = null
+func _process(delta: float) -> void:
+	pass
 
 func generate(count: int) -> Array:
 	var result = []
 	while count > 0:
 		result.append(RandomNumberGenerator.new().randi_range(-128, 127))
 		count -= 1
+		if count > 0:
+			result.append(operators.pick_random())
 	return result
